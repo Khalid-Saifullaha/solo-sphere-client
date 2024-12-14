e9bf43cb802e24c3b212bc2e70a938def9147552
Your Name <khalidsaifullaha3@gmail.com>
2024-12-14 19:47:13 +0600

    add job

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -2,9 +2,14 @@ import { useContext, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { AuthContext } from "../providers/AuthProvider";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const AddJob = () => {
+  const navigate = useNavigate();
   const { user } = useContext(AuthContext);
+
   const [startDate, setStartDate] = useState(new Date());
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -32,18 +37,18 @@ const AddJob = () => {
       bid_count: 0,
     };
     console.log(formData);
-    // try {
-    //   // 1. make a post request
-    //   await axios.post(`${import.meta.env.VITE_API_URL}/add-job`, formData);
-    //   // 2. Reset form
-    //   form.reset();
-    //   // 3. Show toast and navigate
-    //   toast.success("Data Added Successfully!!!");
-    //   navigate("/my-posted-jobs");
-    // } catch (err) {
-    //   console.log(err);
-    //   toast.error(err.message);
-    // }
+    try {
+      // 1. make a post request
+      await axios.post(`${import.meta.env.VITE_API_URL}/add-job`, formData);
+      // 2. Reset form
+      form.reset();
+      // 3. Show toast and navigate
+      toast.success("Data Added Successfully!!!");
+      navigate("/my-posted-jobs");
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
   };
 
   return (
